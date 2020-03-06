acdec5321d42bc9a086fb5dae0a56c83ad080bef
Polina Gubina <33940358+Polina-Gubina@users.noreply.github.com>
2020-03-06 15:41:46 +0300

    Role/build infra (#18)
    * add molecule test
    
    * add installing python-apt to dockerfile
    
    * Add travis integration
    
    * Add missing docker image dependencies
    
    * Fix image building
    
    * Fix molecule test for build_infrastructure role using simple vpc in written in terraform
    
    * Fix for integration in csm
    
    * Fix paths to terraform directory for integration with csm
    
    * Add generating random name for vpc
    
    * Add dependencies between roles
    
    * Auth using clouds.yaml
    
    * Saving before rollback
    
    * Fix for molecule 3.0+
    
    * Merge remote-tracking branch 'origin/role/build-infra' into role/build-infra
    
    * Add clouds.yaml authentification
    
    * Fix line endings
    
    * Change role description
    
    * Correct according by comments on git
    
    * Use existing clouds.yaml instead of own clouds.yaml
    
    * Change name of cloud in openstack provider auth
    
    * Change credentionals travis.yml to *447
    
    Co-authored-by: Polina-Gubina <polina@vrnnb-prc481.t-systems.ru>
    Co-authored-by: Anton Kachurin <anton.kachurin@t-systems.com>

--- build_infrastructure/molecule/default/files/infrastructure/scenario1/infrastructure.tf
+++ build_infrastructure/molecule/default/files/infrastructure/scenario1/infrastructure.tf
@@ -7,7 +7,7 @@ variable "vpc_cidr" {
   default = "192.168.0.0/16"
 }
 
-resource "opentelekomcloud_vpc_v1" "vpc_v3" {
+resource "opentelekomcloud_vpc_v1" "vpc_v1" {
   name = "${random_string.random_name.result}"
   cidr = var.vpc_cidr
 }
